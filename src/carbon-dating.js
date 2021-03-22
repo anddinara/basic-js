2aec98a9bfe45b3565551f0d66da877066a4fd0e
anddinara <and.dinara.moscow@gmail.com>
2021-03-22 23:13:20 +0300

    init: start basic-js-task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,19 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(number) {
+	if (typeof number === 'string') {
+    number = Number(number);
+    if (typeof number === 'number' && !isNaN(number) && number !== 0 && number <= MODERN_ACTIVITY && number > 0) {
+      let k = 0.693 / HALF_LIFE_PERIOD;
+      let age = Math.ceil((Math.log(MODERN_ACTIVITY) - Math.log(number)) / k);
+      return age;
+    } else {
+      return false;
+    }    
+  } else {
+    return false;
+  }
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(arr) {
+	if (typeof arr === 'number' || typeof arr === 'boolean' || arr === null || arr === undefined) {
+    return false;  
+  } else if (typeof arr === 'object' && Object.prototype.toString.call(arr) === '[object Object]') {
+     return false;     
+  }
+  let str = '';
+  arr.forEach((ar) => {
+    console.log(ar);
+    if(typeof ar === 'string') {
+      ar = ar.trim(); 
+      str += ar[0].toUpperCase();
+    }    
+  });
+  console.log(str);
+  return str.split('').sort().join('');
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
